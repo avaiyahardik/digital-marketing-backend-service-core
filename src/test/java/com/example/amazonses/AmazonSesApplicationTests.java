8303cd37f498bdf5108d779c7fefc71c17b3f82c
Hardik Avaiya <hardik.avaiya@affle.com>
2020-10-04 11:16:59 +0530

    Added initial project in got, fresh downloaded from spring initializr

---
+++ src/test/java/com/example/amazonses/AmazonSesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazonses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonSesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
